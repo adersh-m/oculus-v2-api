7af4ad5abd6595ec174ed8dcb05303cfa6877a1d
Adersh M <33145987+adersh-m@users.noreply.github.com>
2022-02-02 19:12:07 +0530

    Uploaded files

---
+++ oculus-v2/api/user/signup.php
@@ -0,0 +1,54 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+// get database connection
+include_once '../config/database.php';
+
+// instantiate user object
+include_once '../objects/user.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$user = new User($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+if (!empty($data->username) && !empty($data->password)) {
+    // set user property values
+    $user->username = $data->username;
+    $user->password = base64_encode($data->password);
+    $user->created = date('Y-m-d H:i:s');
+
+    // create the user
+    if ($user->signup()) {
+        $user_arr = array(
+            "status" => true,
+            "message" => "Successfully Signup!",
+            "id" => $user->id,
+            "username" => $user->username
+        );
+        http_response_code(201);
+        echo json_encode($user_arr);
+    } else {
+        $user_arr = array(
+            "status" => false,
+            "message" => "Username already exists!"
+        );
+        http_response_code(403);
+        echo json_encode($user_arr);
+    }
+} else {
+    $user_arr = array(
+        "status" => false,
+        "message" => "Invalid Request",
+    );
+    http_response_code(400);
+    echo json_encode($user_arr);
+}
